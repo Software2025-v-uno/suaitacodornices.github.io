1809a183b61cbc89e70311ddcfefb09f0f01fcaa
eduproyecto <204776276+Software2025-v-uno@users.noreply.github.com>
2025-12-30 09:59:01 -0500

    subir

---
+++ sw.js
@@ -0,0 +1,44 @@
+// sw.js
+const CACHE_NAME = 'portachuelo-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/img/codorniz.jpg',
+  '/img/logo.png',
+  '/img/logo-192.png',
+  '/img/logo-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('/');
+        }
+      })
+  );
+});
